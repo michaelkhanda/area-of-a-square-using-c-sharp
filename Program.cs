9b759cb3adc013c53c228058bd62c924a7ac673a
Michael Khanda <michaelkhanda006@gmail.com>
2023-06-27 18:53:08 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Areaofasquare
+{
+    class Program
+    {
+        static int Perimeter(out int l)
+        {
+            l = 7;
+            int p = l * 4;
+            return p;
+        }
+        static int SquareArea( out int l)
+        {
+            l = 2;
+            l = l + 2;
+            int area = l * l;
+            return area;
+        }
+        static void Main(string[] args)
+        {
+            int length = 8;
+            int area = SquareArea(out length);
+            Console.WriteLine("The area is: " + area);
+            int p = Perimeter(out length);
+            Console.WriteLine("The area is: " + p);
+            Console.WriteLine("The final length is" +" " + length);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
